83088d3ee6c4165cff63ac072b7122a61ff1d55c
LeandroCaponetto <caponettopeppers@gmail.com>
2022-07-20 13:00:31 -0300

    guado para la entrega

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+function Main() {
+    return(
+        <main>
+
+            <div  id="name">
+                <h1>leandro Caponetto</h1>
+                <h4>programador</h4>
+                <p>full stack</p>
+            </div>
+            
+        </main>
+
+    )
+   
+}
+
+export default Main
